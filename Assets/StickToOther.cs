23f715e6b1fd94605376d35d4aa3b562e9b91478
themanmann <themanmann@gmail.com>
2018-09-09 15:38:39 -0700

    Marshmallow Update
    Updated Marshmallow script to be safer, public Kill method for making marshmallow fall through ground (update kills marshmallow that falls too low)

--- Assets/StickToOther.cs
+++ Assets/StickToOther.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class StickToOther : MonoBehaviour {
 
+    bool alive;
     bool stuck;
 
 	// Use this for initialization
 	void Start () {
+        alive = true;
         stuck = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(transform.position.y <= -4.0)
+        {
+            if(alive)
+            {
+                //lose life
+            }
+
+            Destroy(gameObject);
+        }
 	}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (!stuck && collision.transform.tag.Equals("Sticky"))
         {
-            transform.parent = collision.transform;
+            //transform.parent = collision.transform;
             GetComponent<Rigidbody2D>().isKinematic = true;
             Rigidbody2D[] bodies = GetComponentsInChildren<Rigidbody2D>();
             foreach(Rigidbody2D body in bodies)
@@ -28,10 +38,17 @@ public class StickToOther : MonoBehaviour {
                 body.isKinematic = false;
             }
 
-            /*Transform bottom = collision.gameObject.GetComponent<Transform>();
-            parent = bottom;*/
-
             stuck = true;
+        }
+    }
+
+    void Kill()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+        BoxCollider2D[] boxes = GetComponentsInChildren<BoxCollider2D>();
+        foreach (BoxCollider2D box in boxes)
+        {
+            box.enabled = false;
         }
     }
 }
