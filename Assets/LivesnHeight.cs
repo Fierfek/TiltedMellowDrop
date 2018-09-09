16c23be9bc4131c62902c87f8691c598d8dd1b27
senirusan13 <36501296+senirusan13@users.noreply.github.com>
2018-09-09 13:54:36 -0700

    UI - Counters for height, lives, game over

---
+++ Assets/LivesnHeight.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LivesnHeight : MonoBehaviour {
+    public Text LifeCount;
+    public Text HeightCount;
+    public Text GameOver;
+ 
+    // Use this for initialization
+    int lives;
+    int height;
+	void Start () {
+        height = 0;
+        lives = 3;
+        LifeCount.text = lives.ToString();
+        HeightCount.text = height.ToString();
+        GameOver.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            lives++;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            lives--;
+        }
+        if (lives == 0)
+        {
+            GameOver.text = "Game Over";
+        }
+        LifeCount.text = lives.ToString();
+    }
+}
