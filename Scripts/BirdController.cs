cd7db2eb993632c53562b5293671898a6a4ea7fc
themanmann <themanmann@gmail.com>
2018-09-09 16:47:36 -0700

    I'll figure this out eventually

---
+++ Scripts/BirdController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour {
+    
+    Vector2 targetPos, initPos;
+    float f = 0, duration = 4; // speed at 4 = approx. 4 seconds to pass
+
+	// Use this for initialization
+	void Start () {
+        initPos = transform.position;
+        targetPos = initPos - new Vector2(2 * Camera.main.orthographicSize * Screen.width / Screen.height + 2, 0);
+	}
+
+    // Update is called once per frame
+    void Update () {
+        if (f < 1)
+        {
+            f += Time.deltaTime / duration;
+            transform.position = Vector2.Lerp(initPos, targetPos, f);
+        }
+        else
+            Destroy(gameObject);
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
