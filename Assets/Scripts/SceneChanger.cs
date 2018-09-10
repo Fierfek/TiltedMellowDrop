f69d587bdfe3b485e046991eb644c248c42b2bff
Brennan Zuber <geekssw@yahoo.com>
2018-09-09 21:58:27 -0700

    Menu Finish

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+
+	public void LoadGame() {
+
+		SceneManager.LoadScene("MainGame");
+	}
+
+	public void MainMenu() {
+		SceneManager.LoadScene("StartMenu");
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+
+}
