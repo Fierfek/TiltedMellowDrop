4a6a2aa8bf134ac20c27e530d01062b7bbb342d3
themanmann <themanmann@gmail.com>
2018-09-09 20:48:10 -0700

    Update to CheckMarshmallowDeath
    preparing for merge

--- Assets/Scripts/CheckMarshmallowDeath.cs
+++ Assets/Scripts/CheckMarshmallowDeath.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CheckMarshmallowDeath : MonoBehaviour {
 
@@ -15,7 +16,8 @@ public class CheckMarshmallowDeath : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (lives <= 0)
-            Debug.Log("GAME OVER");
+            //Debug.Log("GAME OVER");
+            SceneManager.LoadScene("GameOver");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
