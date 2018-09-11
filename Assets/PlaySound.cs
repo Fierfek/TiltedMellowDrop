717ce4e724a0d7c74d276333d44c48645997ead9
Brennan Zuber <geekssw@yahoo.com>
2018-09-10 20:12:41 -0700

    Finishing Up

---
+++ Assets/PlaySound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+
+	public AudioClip boomSound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void playBoomSound() {
+
+		AudioSource.PlayClipAtPoint(boomSound, transform.position, 0.75f);
+
+	}
+}
